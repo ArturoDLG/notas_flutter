022e8de27d8a061b17a30f4e7c4bcfb232677927
ArturoDLG <luisarturo101997@hotmail.com>
2023-02-16 21:23:28 -0600

    seccion_2:     - se agrego el archivo correspondiente a la concatenacion

---
+++ seccion_2/bin/concatenacion.dart
@@ -0,0 +1,33 @@
+//Seccion 2: 13. Concatenacion
+// ArturoDLG
+
+void main() {
+  String name = 'Arturo', lastName = 'DLG';
+  int age = 25;
+
+  // concatenar 2 strings
+  String fullName = name + ' ' + lastName;
+  print(fullName);
+
+  // castear un dato a String
+  String resultCast = fullName + ' ' + age.toString();
+  // .toString convierte el entero age a String
+
+  // formatear variables
+  String resultFormat = '$name $lastName, age: $age';
+  print(resultFormat);
+
+  // llamar a metodos dentro de un formateo
+  String lengthLastName = '$lastName length: ${lastName.length}';
+  // usamos {} despues de $ para poder llamar al metodo
+  print(lengthLastName);
+
+  // usar comillas dentro de un string
+  String phrase = 'My name is "$fullName" ';
+  /*
+  otra alternativa a intercalar entre comillas sencillas y dobles es usando
+  \" para que pueden ser imprimidas. 
+  */
+
+  print(phrase);
+}
